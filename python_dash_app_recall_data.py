0890419642f44083e6aceeb4a1e69a278dba1443
Robert Hall <60370199+robertisaachall@users.noreply.github.com>
2022-03-28 10:57:56 -0400

    Refactored code to fit .css class modules.
    Refactored code to fit into class modules where the .css file changes the location and font sizes.

--- python_dash_app_recall_data.py
+++ python_dash_app_recall_data.py
@@ -13,34 +13,91 @@ recall_amount_type = rds.createRecallRemedyCountryData(recall_data, "China")
 recall_columns = list(zip(*recall_amount))
 recall_columns_type = list(zip(*recall_amount_type))
 
-app = dash.Dash(__name__)
+external_stylesheets = [
+    {
+        "rel": "stylesheet",
+    },
+]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    html.H1("Product Recall Analysis"),
-    html.P("Analyze the amount of product recalls based on the number of occurrences by country, and the type of recall breakdown by country."),
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.H1(children="Product Recall Analysis", className="header-title"),
+                html.P(
+                    children="Analyze the amount of product recalls based on the number of occurrences by country, "
+                             "and the type of recall breakdown by country.",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.H1(children=" Frequency for Each Location", className="first_graph_title"),
+                html.P(
+                    children=" Individual product recall data based on individual countries and locations. Bar Graph is interactive to view the data better.",
+                    className="first_graph_description",
+                ),
+            ],
+            className="first_graph",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(id='recall_bar_chart',
+                                       figure={
+                                           "data": [
+                                               {'x': recall_columns[0], 'y': recall_columns[1], 'type': 'bar'}
 
-    html.H1("Product Recall Based on Individual Locations"),
-    html.P("Individual product recall data based on inidivudal countries and locations. Bar Graph is interactive to view the data better."),
-    dcc.Graph(id='recall_bar_chart',
-              figure={
-                  "data": [
-                      {'x': recall_columns[0], 'y': recall_columns[1], 'type': 'bar'}
-
-                  ],
-              }),
-
-    html.H1("Product Recall Type on Specified Location"),
-    html.P("Product Recall Type information (Refund, Replace, Repair, etc) for the specified country or location."),
-    dcc.Graph(id='recall_pie_chart_country',
-              figure={
-                  "data": [
-                      go.Pie(labels=recall_columns_type[0], values= recall_columns_type[1])
-                  ]
-              })
-])
-
-
-
+                                           ],
+                                       }),
+                    className="card",
+                ),
+            ],
+        ),
+        html.Div(
+            children=[
+                html.H1(children=" Recall Type Breakdown by Location", className="second_graph_title"),
+                html.P(
+                    children=" Analyze the amount of product recalls based on the number of occurrences by country, "
+                             "and the type of recall breakdown by country.",
+                    className="second_graph_description",
+                ),
+            ],
+            className="first_graph",
+        ),
+        html.Div(
+            children=[
+                html.Div(children="Region", className="menu-title"),
+                dcc.Dropdown(
+                    id="location-filter",
+                    options=[
+                        {"label": region, "value": region}
+                        for region in recall_columns[0]
+                    ],
+                    value="COUNTRY",
+                    clearable=False,
+                    className="dropdown",
+                ),
+            ]
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(id='recall_pie_chart_country',
+                                       figure={
+                                           "data": [
+                                               go.Pie(labels=recall_columns_type[0], values=recall_columns_type[1])
+                                           ]
+                                       }),
+                    className="card",
+                ),
+            ],
+        ),
+    ]
+)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
